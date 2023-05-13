af1d71548934679634f2509ec18fad9e1d85705f
BLUECALF <89413138+BLUECALF@users.noreply.github.com>
2023-05-13 20:30:22 +0300

    made cart look different in phone

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -7,7 +7,8 @@ function Cart(props) {
   const totalPrice = itemsPrice + shippingPrice;
 
   return (
-        <div className="cart-container">
+    <>
+        <div className="d-none d-lg-block">
           <h2 className="text-center mb-5">Cart</h2>
           {cartItems.length === 0 ? (
             <div className="text-center">Your cart is empty</div>
@@ -57,6 +58,69 @@ function Cart(props) {
             </table>
           )}
         </div>
+        <center>
+        <div className="d-inline-block d-lg-none">
+          <h2 className="text-center mb-5">Cart</h2>
+          {cartItems.length === 0 ? (
+            <div className="text-center">Your cart is empty</div>
+          ) : (
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>amount</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item) => (
+                  <tr key={item.id}>
+                    <td>
+                    <img src={item.image} alt={item.name} style={{ width: '50px', height: '50px' }} />
+                    </td>
+                    <td>{item.name}</td>
+                    <td>${item.price}</td>
+                    <td>
+                      <table>
+                        <tbody>
+                          <tr>
+                        <button className="btn btn-secondary btn-sm" onClick={() => onReduce(item)}>
+                        -
+                      </button>
+                      </tr>
+                      <tr>
+                      <span className="mx-2">{item.amount}</span>
+                      </tr>
+                      <tr>
+                      <button className="btn btn-secondary btn-sm" onClick={() => onAdd(item)}>
+                        +
+                      </button>
+                      </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                    <td>
+                      <button className="btn btn-danger btn-sm" onClick={() => onRemove(item)} >
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan="5" className="text-right">
+                    Total: ${cartItems.reduce((acc, item) => acc + item.price * item.amount, 0)}
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+          )}
+        </div>
+        </center>
+        </>
       );
     };
     
